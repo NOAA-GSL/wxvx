dbcd84e6cc04f947523f7d76f0a897ad412f8aaa
DavidBurrows-NCO <David.Burrows@noaa.gov>
2025-08-14 07:57:47 -0400

    minor bug fix

--- src/wxvx/workflow.py
+++ src/wxvx/workflow.py
@@ -117,12 +117,13 @@ def netcdf_from_prepbufr(c: Config):  # PM Later: Make private.
     taskname = f"netCDF from prepbufr at {yyyymmdd}{hh}"
     yield taskname
     pre = Path("/work/point")  # /gpfs/f6/bil-fire8/world-shared/David.Burrows/wxvx_input/
+    #pre = Path("/gpfs/f6/bil-fire8/world-shared/David.Burrows/wxvx_input/")
     # PM Later: Get input-file info from baseline.url.
     url = c.baseline.url.format(yyyymmdd=yyyymmdd, yyyy=yyyymmdd[:4], mm=yyyymmdd[4:6], hh=hh)
     rundir = c.paths.run / "obs" / yyyymmdd / hh
     netcdf = (rundir / url.split("/")[-1]).with_suffix(".nc")
     config = pre / "pb2nc.config"
-    yield [asset(netcdf, netcdf.is_file) asset(config, config.is_file)]
+    yield [asset(netcdf, netcdf.is_file), asset(config, config.is_file)]
     # PM Later: Consider yielding a task to fetch prepbufr from remote URL.
     # PM Later: Yield a task to *create* config instead of requiring it to exist. 
     prun = _prepbufr_file(rundir, url)
