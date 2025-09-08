8ea4c58c5f4460cd9b28636182880844bd7dc742
Emily Carpenter <137525341+elcarpenterNOAA@users.noreply.github.com>
2025-09-08 13:27:24 -0400

    Update src/wxvx/tests/test_types.py
    Co-authored-by: Paul Madden <136389411+maddenp-cu@users.noreply.github.com>

--- src/wxvx/tests/test_types.py
+++ src/wxvx/tests/test_types.py
@@ -138,8 +138,8 @@ def test_types_Forecast(config_data, forecast):
     assert obj == other1
     other2 = types.Forecast(**{**cfg, "name": "foo"})
     assert obj != other2
-    cfg_no_proj = {**cfg}
-    cfg_no_proj.pop("projection", None)
+    cfg_no_proj = deepcopy(cfg)
+    del cfg_no_proj.pop["projection"]
     default = types.Forecast(**cfg_no_proj)
     assert default.projection == {"proj": "latlon"}
 
